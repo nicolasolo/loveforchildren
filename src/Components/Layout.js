e3f3c10d7e3e62ff2ca2a2cd977e8db201acff7c
nicolasolo <41766950+nicolasolo@users.noreply.github.com>
2019-03-08 00:22:59 +0200

    aaaa

---
+++ src/Components/Layout.js
@@ -0,0 +1,18 @@
+﻿import React, { Component } from 'react';
+import AppBar from './AppBar';
+import Grid from './Grid';
+
+export class Layout extends Component {
+    displayName = Layout.name
+
+    render() {
+        return (
+            <Grid >
+                <AppBar />
+
+                {this.props.children}
+
+            </Grid>
+        );
+    }
+}
