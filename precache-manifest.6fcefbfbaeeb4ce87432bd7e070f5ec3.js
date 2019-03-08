b7a9479eb8219c06bc05d2c8ddfb381e04a0d824
nicolasolo <41766950+nicolasolo@users.noreply.github.com>
2019-03-08 11:16:30 +0200

    Updates

---
+++ precache-manifest.6fcefbfbaeeb4ce87432bd7e070f5ec3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "747b9a0f4cc2a44157eddcc964fcd6fb",
+    "url": "/loveforchildren/static/media/header-bg.747b9a0f.jpg"
+  },
+  {
+    "revision": "0754236d3647bd99cdb5",
+    "url": "/loveforchildren/static/js/runtime~main.651215ee.js"
+  },
+  {
+    "revision": "e22af0c22a86eea4e2b7",
+    "url": "/loveforchildren/static/js/main.71934202.chunk.js"
+  },
+  {
+    "revision": "ec91291021ff18b2b40c",
+    "url": "/loveforchildren/static/js/2.1aeac37d.chunk.js"
+  },
+  {
+    "revision": "e22af0c22a86eea4e2b7",
+    "url": "/loveforchildren/static/css/main.882771a6.chunk.css"
+  },
+  {
+    "revision": "d34e6bf388d3ffd67a224a9b4fe79e08",
+    "url": "/loveforchildren/index.html"
+  }
+];
